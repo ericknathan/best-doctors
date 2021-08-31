a8b5a1664cbcdfe037a77d65eaf0ad079ab59b2d
ericknathan <erick.capito@hotmail.com>
2021-08-30 21:03:34 -0300

    ✅ Create Account Screen finished

--- mobile/src/modules/Auth/screens/CreateAccount/index.tsx
+++ mobile/src/modules/Auth/screens/CreateAccount/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
-import { AuthWrapper, Input, TextLink, Button } from '~/components';
-import { ButtonContainer, ContentContainer } from './styles';
+import { AuthWrapper, Input, Button } from '~/components';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-export const CreateAccountScreen = () => {
-  return <AuthWrapper title="Crie sua conta" />;
+export const CreateAccountScreen = ({
+  navigation,
+}: {
+  navigation: StackNavigationProp<
+    {
+      SelectAccountType: undefined;
+    },
+    'SelectAccountType'
+  >;
+}) => {
+  const goToSelectAccountType = () => {
+    navigation.navigate('SelectAccountType');
+  };
+
+  return (
+    <AuthWrapper
+      title="Crie sua conta"
+      bottomChildren={<Button onPress={goToSelectAccountType}>PRÓXIMO</Button>}>
+      <Input label="NOME" placeholder="Seu nome" />
+      <Input label="EMAIL" mt={16} placeholder="meu.email@exemplo.com" />
+      <Input label="CELULAR" mt={16} placeholder="(XX) X XXXX-XXXX" />
+      <Input label="SENHA" mt={16} placeholder="**************" />
+      <Input label="CONFIRMAR SENHA" mt={16} placeholder="**************" />
+    </AuthWrapper>
+  );
 };
