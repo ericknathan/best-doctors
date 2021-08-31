ec699959e7fb8f78115e3f1f95c0bd7002b0e576
ericknathan <erick.capito@hotmail.com>
2021-08-30 21:11:55 -0300

    🚚 Adding new screens to routes

--- mobile/src/modules/Auth/routes/index.tsx
+++ mobile/src/modules/Auth/routes/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { SplashScreen, LoginScreen, CreateAccountScreen } from '../screens';
+import {
+  SplashScreen,
+  LoginScreen,
+  CreateAccountScreen,
+  SelectAccountTypeScreen,
+  ConfirmPhoneScreen,
+  CreateAccountSuccessScreen,
+} from '../screens';
 
 export const AuthNavigator = () => {
   const Stack = createStackNavigator();
@@ -12,6 +19,15 @@ export const AuthNavigator = () => {
       <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
+      <Stack.Screen
+        name="SelectAccountType"
+        component={SelectAccountTypeScreen}
+      />
+      <Stack.Screen name="ConfirmPhone" component={ConfirmPhoneScreen} />
+      <Stack.Screen
+        name="CreateAccountSuccess"
+        component={CreateAccountSuccessScreen}
+      />
     </Stack.Navigator>
   );
 };
--- mobile/src/modules/Auth/screens/index.ts
+++ mobile/src/modules/Auth/screens/index.ts
@@ -1,3 +1,6 @@
 export * from './Splash';
 export * from './Login';
 export * from './CreateAccount';
+export * from './SelectAccountType';
+export * from './ConfirmPhone';
+export * from './CreateAccountSuccess';
